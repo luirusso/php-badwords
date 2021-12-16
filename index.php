67564f9dc6e3aba2ae39d1a54d33ccaad0da67c5
luirusso <luirusso_@hotmail.it>
2021-12-16 17:07:45 +0100

    last touches

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 
-$test = 'Hello world this is my first try at PHP';
-echo $test;
+$test = '"Hello world this is my first try at PHP"';
+echo 'La frase iniziale è: ' . $test;
 echo '<br>';
-echo strlen($test);
+echo 'La lunghezza della frase è: ' . strlen($test);
 
 echo '<br>';
 $badWord = $_GET["badWord"];
-echo $result = str_replace($badWord, '***', $test);
+echo 'La frase censurata è: ' . $result = str_replace($badWord, '***', $test);
 echo '<br>';
-echo strlen($result);
+echo 'La lunghezza della frase censurata è: ' . strlen($result);
 
 ?>
